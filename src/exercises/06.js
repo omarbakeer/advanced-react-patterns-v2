4540bc23b09245054581cca04e89c4a26db61b24
Omar Bakier <omarbakeer@gmail.com>
2019-01-29 17:44:53 +0200

    exercise 06 completed

--- src/exercises/06.js
+++ src/exercises/06.js
@@ -22,14 +22,18 @@ class Toggle extends React.Component {
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
+  getTogglerProps = ({onClick, ...props}) => ({
+    onClick: (...args) => {
+      onClick && onClick(...args)
+      this.toggle()
+    },
+    ...props,
+  })
   getStateAndHelpers() {
     return {
       on: this.state.on,
       toggle: this.toggle,
-      togglerProps: {
-        'aria-pressed': this.state.on,
-        onClick: this.toggle,
-      },
+      getTogglerProps: this.getTogglerProps,
     }
   }
   render() {
